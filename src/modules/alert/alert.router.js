a13d342630c296f4537087a2a22af2af388da1d3
redbios <as.redbios@gmail.com>
2022-08-18 19:10:51 +0700

    [UPDATE] ready to deploy

--- src/modules/alert/alert.router.js
+++ src/modules/alert/alert.router.js
@@ -4,9 +4,14 @@ import {
   getActions,
   getReasons,
   getMachines,
-  getReasonsByMachine
+  getReasonsByMachine,
 } from "./controllers/master.controller.js";
-import { getAnomaly, updateAnomaly, getAnomalyByMachine } from "./controllers/alert.controller.js";
+import {
+  getAnomaly,
+  updateAnomaly,
+  getAnomalyByMachine,
+  updateReadAlert,
+} from "./controllers/alert.controller.js";
 
 const router = Router();
 
@@ -16,7 +21,8 @@ router.get("/reasons/bymachine/:machineId", getReasonsByMachine);
 router.get("/machines", getMachines);
 
 router.get("/anomalies", getAnomaly);
-router.get("/anomalies/bymachine/:machineId", getAnomalyByMachine)
+router.get("/anomalies/bymachine/:machineId", getAnomalyByMachine);
 router.patch("/anomalies/:id", updateAnomaly);
+router.patch("/anomalies/updateread/:id", updateReadAlert);
 
 export default router;
--- src/modules/alert/controllers/alert.controller.js
+++ src/modules/alert/controllers/alert.controller.js
@@ -1,5 +1,4 @@
 import Anomaly from "../models/anomaly.model.js";
-import Machine from "../models/machine.model.js"
 
 const getAnomaly = async (_, res, next) => {
   try {
@@ -16,42 +15,46 @@ const getAnomaly = async (_, res, next) => {
 
 const getAnomalyByMachine = async (req, res, next) => {
   try {
-    const {machineId} = req.params
+    const { machineId } = req.params;
 
-    const anomalies = await Anomaly.findAll({where: {machineId}, include: ['machine', 'reason']})
+    const anomalies = await Anomaly.findAll({
+      where: { machineId },
+      include: ["machine", "reason"],
+      order: [['id', 'ASC']]
+    });
 
-    const mapedAnomaly = anomalies.map(aml => {
-      const generatedId = String(aml.id + 13210).padStart(8,'0')
-      const reason = aml.reason === null ? {name: 'Unknown Anomally'} : aml.reason
+    const mapedAnomaly = anomalies.map((aml) => {
+      const generatedId = String(aml.id + 13210).padStart(8, "0");
+      const reason =
+        aml.reason === null ? { name: "Unknown Anomally" } : aml.reason;
       return {
         ...aml.dataValues,
         generatedId,
-        reason
-      }
-    })
+        reason,
+      };
+    });
 
     res.status(200).json({
       message: "success",
-      data: mapedAnomaly
-    })
-
+      data: mapedAnomaly,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 const updateAnomaly = async (req, res, next) => {
   try {
     const { id } = req.params;
     const newAnomaly = req.body;
 
-    const anomaly = await Anomaly.findOne({where: {id}})
+    const anomaly = await Anomaly.findOne({ where: { id } });
 
-    anomaly.reasonId = newAnomaly.reasonId
-    anomaly.actionId = newAnomaly.actionId
-    anomaly.comment = newAnomaly.comment
+    anomaly.reasonId = newAnomaly.reasonId;
+    anomaly.actionId = newAnomaly.actionId;
+    anomaly.comment = newAnomaly.comment;
 
-    await anomaly.save()
+    await anomaly.save();
 
     res.status(200).json({
       message: "success",
@@ -61,4 +64,22 @@ const updateAnomaly = async (req, res, next) => {
   }
 };
 
-export { getAnomaly, updateAnomaly, getAnomalyByMachine };
+const updateReadAlert = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const alert = await Anomaly.findOne({ where: { id } });
+
+    alert.isAlreadyOpen = true;
+
+    await alert.save();
+
+    res.status(200).json({
+      message: "success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getAnomaly, updateAnomaly, getAnomalyByMachine, updateReadAlert };
